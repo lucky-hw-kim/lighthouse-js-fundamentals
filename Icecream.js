2a9996316b0675950896c013f818f75184a6b582
Lucky Kim <lucky.hw.kim@gmail.com>
2022-02-04 20:13:09 +0000

    Add packing-list.js : Arry iteration

--- Icecream.js
+++ Icecream.js
@@ -10,3 +10,13 @@ console.log(iceCreamFlavours[0]);
 console.log(iceCreamFlavours[5]);
 console.log(iceCreamFlavours.length)
 
+let sum = 3 + 2;
+
+const doubleSum = function(a, b) {
+  let sum = a + b;
+  return sum * 2;
+}
+
+doubleSum(5, 4);
+
+console.log(sum);
