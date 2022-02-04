61bbd82057d3b460f81099e2e327dc8b206c09a3
Lucky Kim <lucky.hw.kim@gmail.com>
2022-02-04 21:07:48 +0000

    Add looping-range.js & lastIndexOf.js

---
+++ looping-range.js
@@ -0,0 +1,13 @@
+function range (start, end, step){
+
+  let array = [];
+
+  for (let i = start; i <= end; i += step) {
+    array.push(i);
+  }
+  return array;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
